96d7ead490854588a6d29f238e5ea17ceabd18c2
Ngo-Long <ngolong.hh@gmail.com>
2024-10-29 22:52:15 +0700

    hello world

---
+++ src/test/java/com/management/restaurant/ManagementRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
